a6ee32ea75fbe740ccad7c0b988477d094974c56
David Schmitt <david@dasz.at>
2011-11-24 17:59:44 +0100

    Argument checks

--- LinqCube/LinqCube/QueryBuilder.cs
+++ LinqCube/LinqCube/QueryBuilder.cs
@@ -10,6 +10,8 @@ namespace dasz.LinqCube
         public static Query<TFact> WithDimension<TDimension, TFact>(this Query<TFact> q, Dimension<TDimension, TFact> dim)
             where TDimension : IComparable
         {
+            if (q == null) throw new ArgumentNullException("q");
+            if (dim == null) throw new ArgumentNullException("dim");
             if (q.QueryDimensions.FirstOrDefault(i => i.Dimension == (IDimension)dim) != null) throw new InvalidOperationException("Dimension already added");
             q.QueryDimensions.Add(new QueryDimension<TDimension, TFact>(dim));
             return q;
@@ -17,6 +19,8 @@ namespace dasz.LinqCube
 
         public static Query<TFact> WithMeasure<TIntermediate, TFact>(this Query<TFact> q, Measure<TFact, TIntermediate> measure)
         {
+            if (q == null) throw new ArgumentNullException("q");
+            if (measure == null) throw new ArgumentNullException("measure");
             if (q.Measures.Contains(measure))
                 throw new InvalidOperationException("Measure already added");
             q.Measures.Add(measure);
