9b3128cc8d5f6f91d2e361f3386c6ef50aee9377
Arthur Zaczek <arthur.zaczek@gmail.com>
2012-06-06 11:23:07 +0200

    Adding MSBuild Versioning

--- LinqCube/Properties/AssemblyInfo.cs
+++ LinqCube/Properties/AssemblyInfo.cs
@@ -22,15 +22,4 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("0842be3c-bd6f-4d01-bdd9-bfd09a32a306")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
